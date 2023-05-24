4e780e6faa85e87f592d9ea0a08aea9564732995
Veronica De Felice <verodefelic@gmail.com>
2023-05-24 16:13:46 -0400

    hid add and delete

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -21,8 +21,8 @@ function NavMenu() {
           <Nav className="ms-auto ">
             <Nav.Link href="/home">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/login">Admin Login</Nav.Link>
-            <Nav.Link href="/homecrud">Admin Page</Nav.Link>
+            {/* <Nav.Link href="/login">Admin Login</Nav.Link> */}
+            {/* <Nav.Link href="/homecrud">Admin Page</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
